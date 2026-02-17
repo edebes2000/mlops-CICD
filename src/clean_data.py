ba8050955e73f26372f1e3a7a23673575d2814d6
phidesigner <phi.designer@gmail.com>
2026-02-17 20:09:00 +0100

    Add environment.yml to define project dependencies and channels

--- src/clean_data.py
+++ src/clean_data.py
@@ -5,3 +5,6 @@ Role: Preprocessing, missing value imputation, and feature engineering.
 Input: pandas.DataFrame (Raw).
 Output: pandas.DataFrame (Processed/Clean).
 """
+
+if __name__ == "__main__":
+    print("hello")
